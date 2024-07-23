c6f90faf7f7ff73e81f195df161cadf2f60497de
abdullah <abdullah.alanezi.23@gmail.com>
2024-07-23 13:16:20 +0300

    adding my order template

--- ItProject/itsupport/views.py
+++ ItProject/itsupport/views.py
@@ -13,9 +13,16 @@ def support_request(request:HttpRequest):
 
 def user_request_view(request:HttpRequest):
 
+    user_request = MaintenanceRequest.objects.all()
+
     
 
-    return render(request,"itsupport/user_request.html")
+    return render(request,"itsupport/user_request.html",{"users_requests":user_request})
+
+
+def my_order(request:HttpRequest):
+
+    return render(request,"itsupport/my_order.html")
 
 
 
