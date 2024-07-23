4fb82070a49bc029968b246c5cdd8a9d8dff3d16
abdullah <abdullah.alanezi.23@gmail.com>
2024-07-23 08:27:27 +0300

    adding MaintenanceRequest model and prenter model

--- ItProject/itsupport/admin.py
+++ ItProject/itsupport/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import MaintenanceRequest,PrinterRequest
 # Register your models here.
+
+
+admin.site.register(MaintenanceRequest)
+admin.site.register(PrinterRequest)
--- ItProject/itsupport/models.py
+++ ItProject/itsupport/models.py
@@ -1,3 +1,51 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class MaintenanceRequest(models.Model):
+
+    
+    STATUS_CHOICES = [
+        ('sendit', 'تم إرسال الطلب'),
+        ('pending', 'تحت الإجراء'),
+        
+        ('completed', 'منفذ'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    device_type = models.CharField(max_length=255)
+    device_id = models.CharField(max_length=100)
+    problem_description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='تم إرسال الطلب')
+    request_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/',default="images/default.jpg")
+    
+    
+    def __str__(self):
+        return f'{self.device_type} - {self.device_id} ({self.user.username})'
+    
+
+
+
+class PrinterRequest(models.Model):
+
+    
+    STATUS_CHOICES = [
+        ('sendit', 'تم إرسال الطلب'),
+        ('pending', 'تحت الإجراء'),
+        
+        ('completed', 'منفذ'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    printer_type = models.CharField(max_length=255)
+    printer_id = models.CharField(max_length=100)
+    ink_id = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='تم إرسال الطلب')
+    request_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/',default="images/default.jpg")
+    
+    
+    def __str__(self):
+        return f'{self.printer_type} - {self.printer_id} ({self.user.username})'
+
