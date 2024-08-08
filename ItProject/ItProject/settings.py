7b688fa68477dc38bc43a07f39995d04c1bb881f
abdullah <abdullah.alanezi.23@gmail.com>
2024-08-08 14:11:15 +0300

    initilaze project test host

--- ItProject/ItProject/settings.py
+++ ItProject/ItProject/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h(9vxk#5jhbsrlv7wnk$***8z8n*jc!u1sr-01w)307z7c^5n9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['IT']
 
 
 # Application definition
